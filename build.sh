7adddd3b2dcd2c6ed8cb8f37a31893a02ad56cf6
Colin Iverson <coliniverson07@gmail.com>
2026-07-06 18:42:24 -0500

    Adjusting build scripts
    Added build options

--- build.sh
+++ build.sh
@@ -3,7 +3,29 @@
 
 echo "Compiling Source Code"
 ./cpheader.sh
-g++ -c *.cpp -Wall
+g++ -c gcode.cpp -Wall
+g++ -c gcodeparse.cpp -Wall
+g++ -c iocontroller.cpp -Wall
+g++ -c positioncalc.cpp -Wall
+g++ -c programrun.cpp -Wall
+g++ -c stepper.cpp -Wall
+g++ -c vfdcontrol.cpp -Wall
+
+if [$1 == "main" OR $1 == ""]
+g++ -c main.cpp -Wall
+cd ./Build/
+rm tests.o
+cd ..
+fi
+
+if [$1 == "tests"]
+g++ -c tests.cpp -Wall
+cd ./Build/
+rm main.o
+cd ..
+fi
+
+
 cp *.o ./Build/
 rm *.o
 cd Build
