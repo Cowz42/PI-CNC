98aea3bf42536b56103742ce79446088e31c3ddf
Colin Iverson <coliniverson07@gmail.com>
2026-07-07 19:10:29 -0500

    I Missed a p in a file extension
    I hate this game

---
+++ CMakeFiles/pi-cnc.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/colin/Documents/PI-CNC/gcode.cpp" "CMakeFiles/pi-cnc.dir/gcode.cpp.o" "gcc" "CMakeFiles/pi-cnc.dir/gcode.cpp.o.d"
+  "/home/colin/Documents/PI-CNC/gcodeparse.cpp" "CMakeFiles/pi-cnc.dir/gcodeparse.cpp.o" "gcc" "CMakeFiles/pi-cnc.dir/gcodeparse.cpp.o.d"
+  "/home/colin/Documents/PI-CNC/iocontroller.cpp" "CMakeFiles/pi-cnc.dir/iocontroller.cpp.o" "gcc" "CMakeFiles/pi-cnc.dir/iocontroller.cpp.o.d"
+  "/home/colin/Documents/PI-CNC/main.cpp" "CMakeFiles/pi-cnc.dir/main.cpp.o" "gcc" "CMakeFiles/pi-cnc.dir/main.cpp.o.d"
+  "/home/colin/Documents/PI-CNC/positioncalc.cpp" "CMakeFiles/pi-cnc.dir/positioncalc.cpp.o" "gcc" "CMakeFiles/pi-cnc.dir/positioncalc.cpp.o.d"
+  "/home/colin/Documents/PI-CNC/programrun.cpp" "CMakeFiles/pi-cnc.dir/programrun.cpp.o" "gcc" "CMakeFiles/pi-cnc.dir/programrun.cpp.o.d"
+  "/home/colin/Documents/PI-CNC/stepper.cpp" "CMakeFiles/pi-cnc.dir/stepper.cpp.o" "gcc" "CMakeFiles/pi-cnc.dir/stepper.cpp.o.d"
+  "/home/colin/Documents/PI-CNC/vfdcontrol.cpp" "CMakeFiles/pi-cnc.dir/vfdcontrol.cpp.o" "gcc" "CMakeFiles/pi-cnc.dir/vfdcontrol.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
