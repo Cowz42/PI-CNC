40013eb861a40ae07a2ca3f8a1bc1ada1baa7d77
Colin Iverson <coliniverson07@gmail.com>
2026-07-12 20:00:32 -0500

    Include error with multiple definitions
    Next few commits might just be this

--- gcodeinterface.h
+++ gcodeinterface.h
@@ -2,12 +2,12 @@
 
 
 
+#include"gcode.h"
+#include"commands.h"
+
 
 #ifndef GCODE_INTERFACE_H
 #define GCODE_INTERFACE_H
-
-#include"gcode.h"
-#include"commands.h"
 
 // interfaces different command storage methods, converts a GCodeInstruction to a Cmd
 Cmd convertGCode(GCodeInstruction a) {
