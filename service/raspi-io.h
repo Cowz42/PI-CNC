feafb3a551cbc545ea6bf8a3333768d9aa5d50f5
Colin Iverson <coliniverson07@gmail.com>
2026-08-31 13:14:04 -0500

    Working on some headers to compile on my laptop
    hehe

--- service/raspi-io.h
+++ service/raspi-io.h
@@ -2,10 +2,29 @@
 #ifndef __x86_64__
 #include<wiringPi.h>
 #else
+
+#ifndef _RASPI_IO_VIRT_H
+#define _RASPI_IO_VIRT_H
+
+#ifndef OUTPUT
 #define OUTPUT 0
+#endif
+
+#ifndef INPUT
 #define INPUT 1
+#endif
+
+#ifndef LOW
 #define LOW 0
+#endif
+
+#ifndef HIGH
 #define HIGH 1
+#endif
+
 void pinMode(int p, char d) {}
 void digitalWrite(int p, int d) {}
+
+#endif
+
 #endif
