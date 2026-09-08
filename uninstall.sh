e017df7ac82775cadee9539266e93787d5b86ecc
Colin Iverson <coliniverson07@gmail.com>
2026-09-08 14:27:08 -0500

    Making uninstall.sh actually remove the files
    Now it will delete the files after stopping the service, not just leave them forever
    
    Now I'm a good dev
    hehe

--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,8 @@
 
-
+if [[ $(/usr/bin/id -u) -ne 0 ]]; then
+    echo "Not running as root"
+    exit
+fi
 
 echo "Stopping and removing background service"
 
@@ -7,3 +10,6 @@ echo "Stopping and removing background service"
 systemctl disable pi-cnc-service.service
 systemctl stop pi-cnc-service.service
 
+
+rm /etc/systemd/system/pi-cnc-service.service
+rm /usr/sbin/pi-cnc-service-exec
